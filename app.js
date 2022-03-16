c25824d97d1ad1661cc91742794b8c3a27f1426b
jkinisnger <joy.kinsinger02@gmail.com>
2022-03-16 07:28:02 -0500

    sync'd up DB & added actor model via sequelize auto

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var models = require('./models')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,5 +39,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+models.sequelize.sync().then(function(){
+  console.log("DB Sync'd up")
+})
 
 module.exports = app;
